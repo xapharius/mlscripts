d66731d147fc34b96b026d69281410c052683c8e
xapharius <mihai.suteu@xapharius.com>
2020-08-09 14:43:44 +0100

    set pytorch seed utility func

---
+++ my_utils.py
@@ -0,0 +1,21 @@
+import os
+import random
+import numpy as np
+
+
+
+def set_torch_seed(seed: int = 0):
+    """
+    Set the random seed for torch (both cpu and gpu)
+    """
+    import torch
+
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    
